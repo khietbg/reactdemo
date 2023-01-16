be11ef24fbfe370cc29a717e7c1c54eb2b025c0d
minh khiet <minhkhiet14151993@gmail.com>
2023-01-16 21:49:50 +0700

    reactdemo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderSection from './components/HeaderSection';
+import SliderSection from './components/SliderSection';
+import DelailSection from './components/DelailSection';
+import ProductSection from './components/ProductSection';
+import FindSection from './components/FindSection';
+import ClientSection from './components/ClientSection';
+import Sign from './components/Sign';
+import Info from './components/Info';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+  <div className="hero_area">
+    {/* header section strats */}
+   <HeaderSection></HeaderSection>
+
+    {/* end header section */}
+    {/* slider section */}
+   <SliderSection></SliderSection>
+    {/* end slider section */}
+  </div>
+  {/* detail section */}
+  <DelailSection></DelailSection>
+  {/* end detail section */}
+  {/* products section */}
+ <ProductSection></ProductSection>
+  {/* end products section */}
+  {/* find section */}
+ <FindSection></FindSection>
+  {/* end find section */}
+  {/* client section */}
+  <ClientSection></ClientSection>
+  {/* end client section */}
+  {/* sign section */}
+ <Sign></Sign>
+  {/* end sign section */}
+  {/* info section */}
+  <Info></Info>
+  {/* end info section */}
+  {/* footer section */}
+<Footer></Footer>
+</>
+
     </div>
   );
 }
